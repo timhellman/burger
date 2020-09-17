662201926f3023412fa34e6bb1b9e6cf701356f0
Tim Hellman <timhellman@yahoo.com>
2020-09-17 14:57:34 -0700

    fixed password history issue thanks to bryan

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+require('dotenv').config();
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.sql_pw,
+  database: "burgers_db"
+});
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+module.exports = connection
